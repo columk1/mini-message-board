3b180bf150198df6400b810adf84a723adf0e3fb
Colum Kelly <columk1@users.noreply.github.com>
2023-11-17 22:52:27 -0800

    Add new message form and /new route

--- routes/index.js
+++ routes/index.js
@@ -15,8 +15,23 @@ const express = require('express')
 const router = express.Router()
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', (req, res) => {
   res.render('index', { title: 'Mini Message Board', messages: messages })
 })
+
+router
+  .get('/new', (req, res) => {
+    res.render('form')
+  })
+  .post('/new', (req, res) => {
+    console.log(req.body.text)
+    let newMessage = {
+      text: req.body.message,
+      user: req.body.name,
+      added: new Date(),
+    }
+    messages.push(newMessage)
+    res.redirect('/')
+  })
 
 module.exports = router
